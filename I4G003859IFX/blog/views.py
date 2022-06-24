dc0036e81e8b2f663705bf78618aa1872e5ad038
Samuel Chimezie <samuelogoegbu@gmail.com>
2022-06-24 20:43:05 +0100

    Still figuring

--- I4G003859IFX/blog/views.py
+++ I4G003859IFX/blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from .models import Post
 
 
@@ -30,6 +31,8 @@ class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url: reverse_lazy("blog:all")
+
+
 
 
 
